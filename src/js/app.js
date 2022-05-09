9275aae691de191964b65b226aefb59b7f963c21
lleevv2030@gmail.com <lleevv2020@mail.com>
2022-05-09 17:04:19 +0400

    add

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+//import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import * as flsFunctions from "./modules/functions.js";
+
+
+flsFunctions.isWebp();
+
+// core version + navigation, pagination modules:
+/*import Swiper, {
+	Navigation,
+	Pagination
+} from 'swiper';
+
+// init Swiper:
+const swiper = new Swiper();*/

ed0179a503895a8207195a8211a8f9e0b2380013
LLEEVV2020 <lleevv2020@gmail.com>
2022-05-10 17:59:41 +0400

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,21 @@
 //import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import './jquery.min.js';
+import './jquery-migrate-3.0.1.min.js';
+import './popper.min.js';
+import './bootstrap.min.js';
+import './jquery.easing.1.3.js';
+import './jquery.waypoints.min.js';
+import './jquery.stellar.min.js';
+import './jquery.animateNumber.min.js';
+import './owl.carousel.min.js';
+import './jquery.magnific-popup.min.js';
+import './scrollax.min.js';
+import './google-map-js.js';
+import './google-map.js';
+import './main.js';
+
+
+
 import * as flsFunctions from "./modules/functions.js";
 
 
